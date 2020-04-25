b41a15c1597e66e96a5028533744fc05c358f25a
Ben Okorie <anthonycbn@yahoo.com>
2020-04-25 13:30:47 +0100

    Second Assignment

---
+++ Common.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace testing
+{
+    public class Mentor : Student
+    {
+        public int YearsOfExperience { get; set; }
+        public string Category { get; set; }
+
+    }
+    public class Student : Common
+    {
+        public int NumberOfCourses { get; set; }
+    }
+    public class Common
+    {
+        public string Name { get; set; }
+        public bool RegisteredOnZoom { get; set; }
+        public string Location { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mentor mentor1 = new Mentor()
+            {
+                Name = "Ayo",
+                Location = "Canada",
+                YearsOfExperience = 10,
+                Category = "Coding",
+                RegisteredOnZoom = true,
+            };
+
+            Mentor mentor2 = new Mentor()
+            {
+                Name = "Ben",
+                Location = "Nigeria",
+                YearsOfExperience = 4,
+                Category = "Ux/UI",
+                RegisteredOnZoom = false,
+            };
+
+            List<Mentor> MentorsList = new List<Mentor>();
+            MentorsList.Add(mentor1);
+            MentorsList.Add(mentor2);
+
+            foreach (Mentor m in MentorsList)
+            {
+                Console.WriteLine("Our Mentor is {0}, he is lives in {1}, and has {2} years of experience", m.Name, m.Location, m.YearsOfExperience);
+            }
+        }
+
+    }
+
+}
